59d7ad41cea7de9d73435ab0caf616dc5e17de0c
shtaka <shtaka@mail.ru>
2011-02-27 19:06:04 +0200

    S

---
+++ Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace SPOProj
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 73);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "VideoCard";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(108, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(120, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "NetWorkConnection";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(247, 11);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(115, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Identification";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(386, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(115, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "BootConfig";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(528, 12);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(135, 23);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "ComputerSystem";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(684, 12);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(98, 23);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "CompSystem2";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(789, 13);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(80, 23);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "Desktop";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(15, 43);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 23);
+            this.button8.TabIndex = 8;
+            this.button8.Text = "DiskParts";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(881, 362);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Management;
+
+namespace SPOProj
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           label1.Text = VideoCard();
+        }
+        private string VideoCard()
+        {
+            ManagementScope sc = new ManagementScope(@"\\.\root\cimv2", null);
+            ManagementPath ph = new ManagementPath(@"Win32_VideoController");
+            ManagementClass mc = new ManagementClass(sc, ph, null);
+            string Vid = "null";
+            foreach (ManagementObject ss in mc.GetInstances())
+            {
+                try
+                {
+                     Vid ="Name : " + ss.GetPropertyValue("Name").ToString() + '\n'
+                   + "VideoProcessor : " + ss.GetPropertyValue("VideoProcessor").ToString() + '\n'
+                   + "AdapterRAM : " + ss.GetPropertyValue("AdapterRAM").ToString() + '\n'
+                   + "VideoModeDescription : " + ss.GetPropertyValue("VideoModeDescription").ToString() + '\n'
+                   + "CurrentRefreshRate : " + ss.GetPropertyValue("CurrentRefreshRate").ToString();
+                }
+                catch (NullReferenceException ex)
+                {
+                }
+            }
+            return Vid;
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+           label1.Text =  NetworkConnect();
+        }
+        private string NetworkConnect()
+        {
+            ManagementClass managementClass = new ManagementClass("Win32_NetworkConnection");
+            ManagementObjectCollection managementObj = managementClass.GetInstances();
+            string ret = "Null";
+            foreach (ManagementObject mo in managementObj)
+            {
+
+               ret = "AccessMask: " + mo["AccessMask"].ToString() + '\n' +
+               "AccessMask: " + mo["AccessMask"] + '\n' +
+                "Caption: " + mo["Caption"] + '\n' +
+                "Comment: " + mo["Comment"] + '\n' +
+                "ConnectionState: " + mo["ConnectionState"] + '\n' +
+                "ConnectionType: " + mo["ConnectionType"] + '\n' +
+                "Description: " + mo["Description"] + '\n' +
+                "DisplayType: " + mo["DisplayType"] + '\n' +
+                "InstallDate: " + mo["InstallDate"] + '\n' +
+                "LocalName: " + mo["LocalName"] + '\n' +
+                "Name: " + mo["Name"] + '\n' +
+                "Persistent: " + mo["Persistent"] + '\n' +
+                "ProviderName: " + mo["ProviderName"] + '\n' +
+                "RemoteName: " + mo["RemoteName"] + '\n' +
+                "RemotePath: " + mo["RemotePath"] + '\n' +
+                "ResourceType: " + mo["ResourceType"] + '\n' +
+                "Status: " + mo["Status"] + '\n' +
+                "UserName: " + mo["UserName"];
+            }
+            return ret;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label1.Text = Ident();
+        }
+        private string Ident()
+        {
+            WqlObjectQuery query = new WqlObjectQuery("SELECT * FROM Win32_ComputerSystemProduct");
+            ManagementObjectSearcher find = new ManagementObjectSearcher(query);
+            string ret = "null";
+            foreach (ManagementObject mo in find.Get())
+            {
+                ret = "Description." + mo["Description"] + '\n' +
+                "Identifying number (usually serial number)." + mo["IdentifyingNumber"] + '\n' +
+                "Commonly used product name." + mo["Name"] + '\n' +
+                "Universally Unique Identifier of  product." + mo["UUID"] + '\n' +
+                "Vendor of product." + mo["Vendor"];
+            }
+            return ret;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            label1.Text = Boot();
+        }
+        private string Boot()
+        {
+            string ret = "null";
+            WqlObjectQuery query = new WqlObjectQuery(
+                "SELECT * FROM Win32_BootConfiguration");
+            ManagementObjectSearcher find =
+                new ManagementObjectSearcher(query);
+            foreach (ManagementObject mo in find.Get())
+            {
+                ret = "Boot directory with files required for booting." + mo["BootDirectory"] + '\n' +
+                "Description." + mo["Description"] + '\n' +
+                "Directory with temporary files for booting." + mo["ScratchDirectory"] + '\n' +
+                "Directory with temporary files." + mo["TempDirectory"];
+            }
+            return ret;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            label1.Text = CompSys();
+        }
+        private string CompSys()
+        {
+            string ret="null";
+
+            WqlObjectQuery query = new WqlObjectQuery("SELECT * FROM Win32_ComputerSystem");
+            ManagementObjectSearcher find = new ManagementObjectSearcher(query);
+            foreach (ManagementObject mo in find.Get())
+            {
+                ret = "Computer belongs to domain " + mo["Domain"] + '\n' +
+                "Computer manufacturer." + mo["Manufacturer"] + '\n' +
+                "Model name given by manufacturer " + mo["Model"];
+            }
+
+            return ret;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            label1.Text = CompSys2();
+        }
+        private string CompSys2()
+        {
+            string ret = "Null";
+            string[] Roles = {
+								 "Standalone Workstation", // 0
+								 "Member Workstation",  // 1
+								 "Standalone Server",  // 2
+								 "Member Server",   // 3
+								 "Backup Domain Controller", // 4
+								 "Primary Domain Controller" // 5
+							 };
+
+            WqlObjectQuery query = new WqlObjectQuery(
+                "SELECT * FROM Win32_ComputerSystem");
+            ManagementObjectSearcher find =
+                new ManagementObjectSearcher(query);
+            foreach (ManagementObject mo in find.Get())
+            {
+                ret =Roles[Convert.ToInt32(mo["DomainRole"])];
+            }
+            return ret;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            label1.Text = Desktop();
+        }
+        public string Desktop()
+        {
+            string ret = "Null";
+            // Получить настройки рабочего стола
+            WqlObjectQuery query = new WqlObjectQuery(
+                "SELECT * FROM Win32_Desktop WHERE Name = '.Default'");
+            ManagementObjectSearcher find =
+                new ManagementObjectSearcher(query);
+            foreach (ManagementObject mo in find.Get())
+            {
+                // Значения могут быть изменены 
+                // в реестре "HKEY_CURRENT_USER\Control Panel\Desktop"
+                ret = "Width of window borders." +  mo["BorderWidth"] + '\n' +
+                "ALT+TAB task switching allowed." + mo["CoolSwitch"] + '\n' +
+                // Значения в мс
+                "Lenght of time between cursor blincks. " + mo["CursorBlinkRate"] + '\n' +
+                "Show content of windows when are draged." + mo["DragFullWindows"] + '\n' +
+                "Grid settings for dragging windows." + mo["GridGranularity"] + '\n' +
+                "Grid settings for icon spacing. " + mo["IconSpacing"] + '\n' +
+                "Font used for the names of icons." + mo["IconTitleFaceName"] + '\n' +
+                "Icon ront size. " + mo["IconTitleSize"] + '\n' +
+                "Wrapping of icon title." + mo["IconTitleWrap"] + '\n' +
+               "Name of the desktop profile." + mo["Name"] + '\n' +
+                "Screen saver is active." + mo["ScreenSaverActive"] + '\n' +
+               "Name of the screen saver executable." + mo["ScreenSaverExecutable"] + '\n' +
+                "Is screen saver protected with password." + mo["ScreenSaverSecure"] + '\n' +
+                "Time to pass to activate screen saver." + mo["ScreenSaverTimeout"] + '\n' +
+                "File name for desktop background." + mo["Wallpaper"] + '\n' +
+                "Wallpaper fills entire screen." + mo["WallpaperStretched"] + '\n' +
+                "Wallpaper is tiled." + mo["WallpaperTiled"];
+            }
+            return ret;
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            label1.Text = DiskParts();
+        }
+        private string DiskParts()
+        {
+            string ret = "Null";
+            WqlObjectQuery query = new WqlObjectQuery(
+                "Select * from Win32_DiskPartition");
+            ManagementObjectSearcher find =
+                new ManagementObjectSearcher(query);
+            foreach (ManagementObject mo in find.Get())
+            {
+                ret = "Block size." + mo["BlockSize"] + " Bytes" + '\n' +
+                "Partition is labeled as bootable. " + mo["Bootable"] + '\n' +
+                "Boot partition active. " + mo["BootPartition"] + '\n' +
+                "Caption.." + mo["Caption"] + '\n' +
+                "Description." + mo["Description"] + '\n' +
+                "Unique identification of partition.." + mo["DeviceID"] + '\n' +
+                "Index number of the disk with that partition." + mo["DiskIndex"] + '\n' +
+                "Detailed description of error in LastErrorCode." + mo["ErrorDescription"] + '\n' +
+                "Type of error detection and correction." + mo["ErrorMethodology"] + '\n' +
+                "Hidden sectors in partition." + mo["HiddenSectors"] + '\n' +
+                "Index number of the partition." + mo["Index"] + '\n' +
+                "Last error by device." + mo["LastErrorCode"] + '\n' +
+                "Total number of consecutive blocks." + mo["NumberOfBlocks"] + '\n' +
+                "Partition labeled as primary." + mo["PrimaryPartition"] + '\n' +
+                "Free description of media purpose. " + mo["Purpose"] + '\n' +
+                "Total size of partition." + mo["Size"] + " bytes" + '\n' +
+                "Starting offset of the partition " + mo["StartingOffset"] + '\n' +
+                "Status." + mo["Status"] + '\n' +
+                "Type of the partition." + mo["Type"];
+            }
+            return ret;
+        }
+    }
+
+        
+}
